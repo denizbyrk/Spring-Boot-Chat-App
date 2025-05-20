fec399c7ff64be18125981c3e792f367c7230289
DenizBayrak <deniz.bayrak7580@gmail.com>
2025-05-21 02:08:27 +0300

    Updated WebSocket Controllers

--- chat-app/src/main/java/com/chatapp/controller/WebSocketChatController.java
+++ chat-app/src/main/java/com/chatapp/controller/WebSocketChatController.java
@@ -1,5 +1,41 @@
 package com.chatapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+
+import com.chatapp.model.Message;
+import com.chatapp.model.User;
+import com.chatapp.service.MessageService;
+import com.chatapp.service.UserService;
+
+@Controller
 public class WebSocketChatController {
 
+    @Autowired
+    private MessageService messageService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat.send")
+    public void send(Message message, Authentication authentication) {
+    	
+        User sender = userService.findByUsername(authentication.getName())
+                .orElseThrow(() -> new RuntimeException("Sender not found"));
+        User receiver = userService.findById(message.getReceiver().getId());
+
+        messageService.sendMessage(sender, receiver, message.getContent());
+
+        message.setSender(sender);
+        message.setReceiver(receiver);
+
+        messagingTemplate.convertAndSendToUser(sender.getUsername(), "/queue/messages", message);
+        messagingTemplate.convertAndSendToUser(receiver.getUsername(), "/queue/messages", message);
+    }
 }
--- chat-app/src/main/java/com/chatapp/controller/WebSocketMessageController.java
+++ chat-app/src/main/java/com/chatapp/controller/WebSocketMessageController.java
@@ -1,5 +1,31 @@
 package com.chatapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+import com.chatapp.model.Message;
+import com.chatapp.model.User;
+import com.chatapp.service.MessageService;
+import com.chatapp.service.UserService;
+
+@Controller
 public class WebSocketMessageController {
 
+    @Autowired
+    private MessageService messageService;
+
+    @Autowired
+    private UserService userService;
+
+    @MessageMapping("/chat")
+    public Message handleMessage(Message message) {
+
+        User sender = userService.findById(message.getSender().getId());
+        User receiver = userService.findById(message.getReceiver().getId());
+
+        messageService.sendMessage(sender, receiver, message.getContent());
+
+        return message;
+    }
 }
